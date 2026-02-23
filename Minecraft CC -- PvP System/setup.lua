3fb947d4341a96036e15edda4c7b00f397a6f95b
KosaGames <timbuescher02@gmail.com>
2026-02-23 19:21:31 +0100

    - removed manuel token input

--- Minecraft CC -- PvP System/setup.lua
+++ Minecraft CC -- PvP System/setup.lua
@@ -13,12 +13,9 @@ local function download(url, path)
     end
 end
 
-write("Token: ")
-local token = read()
-
-download("https://raw.githubusercontent.com/KosaGames/CCTweaked-Programs/refs/heads/main/utils/ui.lua?token="..token, "utils/ui.lua")
-download("https://raw.githubusercontent.com/KosaGames/CCTweaked-Programs/refs/heads/main/utils/db.lua?token="..token, "utils/db.lua")
-download("https://raw.githubusercontent.com/KosaGames/CCTweaked-Programs/refs/heads/main/Minecraft%20CC%20--%20PvP%20System/pvp_guis.lua?token="..token, "pvp_guis.lua")
-download("https://raw.githubusercontent.com/KosaGames/CCTweaked-Programs/refs/heads/main/Minecraft%20CC%20--%20PvP%20System/startup.lua?token="..token, "startup.lua")
+download("https://raw.githubusercontent.com/KosaGames/CCTweaked-Programs/refs/heads/main/utils/ui.lua?token=GHSAT0AAAAAADWEUVWYU4BAQZKNBDK5WVJU2M4TKVA", "utils/ui.lua")
+download("https://raw.githubusercontent.com/KosaGames/CCTweaked-Programs/refs/heads/main/utils/db.lua?token=GHSAT0AAAAAADWEUVWZIDEL7PI234RUK5V42M4TKMQ", "utils/db.lua")
+download("https://raw.githubusercontent.com/KosaGames/CCTweaked-Programs/refs/heads/main/Minecraft%20CC%20--%20PvP%20System/pvp_guis.lua?token=GHSAT0AAAAAADWEUVWYR4HKAANIVUK7YKRC2M4TJHA", "pvp_guis.lua")
+download("https://raw.githubusercontent.com/KosaGames/CCTweaked-Programs/refs/heads/main/Minecraft%20CC%20--%20PvP%20System/startup.lua?token=GHSAT0AAAAAADWEUVWZXRJWKOIROKX7JEJG2M4TJYA", "startup.lua")
 
 print("Installation abgeschlossen!")
